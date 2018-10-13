ca944cf50ac7c19107c483b55afedbf383e7a650
stiero <hensontauro@yahoo.com>
2018-10-14 00:11:32 +0530

    .

--- stacking.py
+++ stacking.py
@@ -45,7 +45,7 @@ list_rf = []
 
 metrics_rf = {}
 
-rf = RandomForestClassifier(n_estimators = 100, random_state = 50, verbose = 1,
+rf = RandomForestClassifier(n_estimators = 5000, random_state = 50, verbose = 1,
                                        n_jobs = -1, oob_score=True)
 
 #del train['target']
@@ -90,7 +90,7 @@ params = {'max_depth': 2, 'eta': 0.5, 'silent': 0, 'objective': 'binary:logistic
 
 evallist = [(dtest, 'eval'), (dtrain, 'train')]
 
-num_rounds = 100
+num_rounds = 5000
 
 
 bst = xgb.train(params, dtrain, num_rounds, evallist)
@@ -133,15 +133,73 @@ list_xgb.append(metrics_xgb)
 
 #################################################
 
-ens = VotingClassifier(estimators=[('gnb', gnb), ('rf', rf), ('xgb', xgb)])
+from lightgbm import LGBMClassifier
 
-ens.fit(X_train, y_train)
+list_lgb = []
 
-ens.score(X_test, y_test)
+metrics_lgb = {}
+
+lgb = LGBMClassifier(n_estimators=5000, objective='binary', class_weight='balanced',
+                     learning_rate=0.005, reg_alpha=0.5, reg_lambda=0.3, subsample=0.8,
+                     n_jobs=-1, random_state=50)
+
+lgb.fit(X_train, y_train, eval_metric='auc', verbose=True)
+
+lgb_pred = lgb.predict(X_test)
+#lgb_pred_prob = lgb.predict_proba(X_test)[:,1]
+
+threshold = 0.5
+
+#lgb_pred = lgb_pred_prob > threshold
+
+accuracy = accuracy_score(y_test, lgb_pred)
+metrics_lgb['accuracy'] = accuracy
+    
+roc = roc_auc_score(y_test, lgb_pred)
+metrics_lgb['auc_roc'] = roc
+    
+kappa = cohen_kappa_score(y_test, lgb_pred)
+metrics_lgb['kappa'] = kappa
+
+conf_matrix = confusion_matrix(y_test, lgb_pred)
+metrics_lgb['conf_matrix'] = conf_matrix
+
+list_lgb.append(metrics_lgb)
+
+###################################################
+
+from sklearn.svm import SVC
+
+list_svm = []
+
+svc = SVC(C=1, kernel='rbf', degree=3)
+
+svc.fit(X_train, y_train)
+
+svc_pred = svc.predict(X_test)
+
+metrics_svm = {}
+
+metrics_svm['accuracy'] = accuracy(y_test, svc_pred)
+metrics_svm['auc_roc'] = roc_auc_score(y_test, svc_pred)
+metrics_svm['kappa'] = cohen_kappa_score(y_test, svc_pred)
+metrics_svm['conf_matrix'] = confusion_matrix(y_test, svc_pred)
+
+list_svm.append(metrics_svm)
+
+########################################################
+
+#ens = VotingClassifier(estimators=[('gnb', gnb), ('rf', rf), ('xgb', xgb)])
+
+#ens.fit(X_train, y_train)
+
+#ens.score(X_test, y_test)
 
 final_pred = np.array([])
 for i in range(len(X_test)):
-    final_pred = np.append(final_pred, mode([gnb_pred[i], rf_pred[i], gnb_pred[i]])[0].item())
+    final_pred = np.append(final_pred, mode([gnb_pred[i], rf_pred[i], 
+                                             xgb_pred[i], lgb_pred[i],
+                                             svc_pred[i]])[0].item())
     
 
 list_final = []
